affda0870787d51a72bac5e26e78454794ff612c
Jonathan Johnson <jonathanjohnson@iheartmedia.com>
2018-04-29 11:54:31 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+./gradlew jar
+sudo mv build/libs/HandheldRainbow.jar /usr/local/bin/HandheldRainbow.jar
+sudo cp linux-service/handheld-rainbow /etc/init.d/handheld-rainbow
+sudo update-rc.d handheld-rainbow defaults
+mkdir /home/pi/monitors
+mkdir /home/pi/RainbowHatInterface
+cp linux-service/*.py /home/pi/RainbowHatInterface/
