15eaf2677fbe39b32c7da43ee007f7c143adb08f
Jonathan Johnson <jonathanjohnson@iheartmedia.com>
2018-05-02 21:32:40 -0400

    update displayValues

--- linux-service/displayValues.py
+++ linux-service/displayValues.py
@@ -6,12 +6,15 @@ u.clear()
 
 
 for index in range(1, len(sys.argv)):
-    rgbValue = sys.argv[index]
+    rgbValue = int(sys.argv[index])
     diodeIndex = index - 1
-    x = diodeIndex % 8
-    y = diodeIndex / 8
-    r = rgbValue & 0xFF0000
-    g = rgbValue & 0x00FF00
+    y = diodeIndex % 8
+    x = diodeIndex / 8
+    r = (rgbValue & 0xFF0000) >> 16
+    g = (rgbValue & 0x00FF00) >> 8
     b = rgbValue & 0x0000FF
+    #print "value {} {} index {} x {} y {} r {} g {} b {}".format(rgbValue, hex(rgbValue), index, x, y, r, g,b)
     u.set_pixel(x, y, r, g, b)
 u.show()
+
+
